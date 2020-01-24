3c2c53be5ac4dee57c93d19d73c1f3b10c57ee82
xbbnxnh <yasmine.ayad@bnymellon.com>
2020-01-24 14:06:37 -0500

    response

--- api/app.js
+++ api/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const router = require('./src/router');
 const path = require('path');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/', router)
+app.use(express.static(path.resolve(__dirname, 'uploads')));
 app.use('/*', (request, response) => {
   response.sendFile(pathToIndex);
   });
---
+++ api/src/router.js
@@ -0,0 +1,38 @@
+const {Router} = require('express');
+const multer = reqire('multer');
+const router = Router();
+
+const filename = (request, file, callback) => {
+  callback(null, file.originalname)
+}
+
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename,
+});
+
+const fileFiler = (request, file, callback) => {
+  if (file.mimetype != 'image/png'){
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+};
+
+const upload = multer({
+  fileFiler,
+  storage
+});
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+  if (request.fileValidationError) {
+    return response.status(400).json({error: request.fileValidationError});
+  }
+
+  return response.status(201).json({success: true});
+})
+
+module.exports = router;
+
+
